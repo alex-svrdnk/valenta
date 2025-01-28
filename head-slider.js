0e6372b606eeb2cc18fc15d1ef7c0c7c454ddaff
alex-svrdnk <accou.ratten@gmail.com>
2025-01-29 00:20:36 +0100

    prettifying slider

--- head-slider.js
+++ head-slider.js
@@ -1,12 +1,9 @@
 $(() => {
-    let bg = '';
-    $('.main-section-item').each(function(){
-        $(this).on('click', function(){
-           bg = $(this).data('bg');
-           $('.head-slide').each(function(){
-               $(this).removeClass('active-head-slide');
-           });
-           $(`.head-slide[data-bg="${bg}"]`).addClass('active-head-slide');
-        })
-    })
-})
+    $('.main-section-item').on('click', function () {
+        const bg = $(this).data('bg');
+        $('.head-slide').removeClass('active-head-slide');
+        $('.main-section-item').removeClass('selected-menu-item');
+        $(`.head-slide[data-bg="${bg}"]`).addClass('active-head-slide');
+        $(this).addClass('selected-menu-item');
+    });
+});
