d6109503585d006f65d1cbc1ab5c0faf39984f1e
alex-svrdnk <accou.ratten@gmail.com>
2025-01-31 07:32:47 +0100

    close menu when how-to-get clicked

--- o_nas.js
+++ o_nas.js
@@ -1,8 +1,12 @@
-const menu = document.querySelector('#nav-links')
+const menu = document.querySelector('#nav-links');
+
+function closeMenu(){
+    menu.classList.remove('active');
+}
 
 document.querySelector('#mobile-menu').addEventListener('click', function(){
     menu.classList.add('active');
 });
-document.querySelector('.close-nav').onclick = () => {
-    menu.classList.remove('active');
-}
+document.querySelector('.close-nav').addEventListener('click', closeMenu)
+
+document.querySelector('#how-to-get-link').addEventListener('click', closeMenu);
